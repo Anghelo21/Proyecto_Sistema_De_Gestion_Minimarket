20e3a61edb90e31bcd4ee2f7f366ba58cbe37a72
Samuel Gómez <samusmusic18@gmail.com>
2025-12-14 18:14:23 -0500

    Agregando el gestionamiento de clientes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char** argv) {
+#include "Persona.h"
+using namespace std;
+int main() {
+	int op = 0;
+	while(op!=2){
+		cout<<"---------MiniMarket---------"<<endl;
+		cout<<"1) Gestion de clientes"<<endl;
+		cout<<"2) Salir"<<endl;
+		cout<<"Elija una opcion"<<endl;
+		cin>>op;
+		switch(op){
+			case 1:
+				gestionClientes();
+				break;
+			case 2:
+				cout<<"Saliendo..."<<endl;
+				break;
+			default:
+				cout<<"Opcion invalida"<<endl;
+		}
+	}
 	return 0;
 }
