f170d02865c7de950e2375b27ff09d6507bfbef4
AngheloCM <angheloprog@gmail.com>
2025-12-18 11:42:42 -0500

    Añadiendo funciones Producto.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,5 @@
 #include <iostream>
-#include "Persona.h"
 using namespace std;
 int main() {
-	int op = 0;
-	while(op!=2){
-		cout<<"---------MiniMarket---------"<<endl;
-		cout<<"1) Gestion de clientes"<<endl;
-		cout<<"2) Salir"<<endl;
-		cout<<"Elija una opcion"<<endl;
-		cin>>op;
-		switch(op){
-			case 1:
-				gestionClientes();
-				break;
-			case 2:
-				cout<<"Saliendo..."<<endl;
-				break;
-			default:
-				cout<<"Opcion invalida"<<endl;
-		}
-	}
 	return 0;
 }
